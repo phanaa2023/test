69e12255a04dc7bb9ae53f639749248c79f4a557
phanaa2023 <phanaa2023@gmail.com>
2026-07-15 16:28:34 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
        Service Worker v2
     ================================================== */
 
-    const CACHE_NAME = "mot-goc-phat-phap-v2";
+    const CACHE_NAME = "mot-goc-phat-phap-v2.1";
 
 /* ==================================================
    Cài đặt
